0e2499e5ae20215195175419ff9c80fa81e12cf4
Kevin01the1novator <97161588+Kevin01the1novator@users.noreply.github.com>
2024-09-20 16:30:28 +0700

    Delete SDN_Controller.m

--- SDN_Controller.m
+++
@@ -1,7 +0,0 @@
-function SDN_Controller(adjMatrix, event)
-    disp('SDN Controller received an event:');
-    disp(event);
-
-    affectedNodes = event.affectedNodes;
-    recomputePartial(adjMatrix, affectedNodes);
-end
